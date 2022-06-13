fcd01916389c49796b5d59189d0e0825d9b29e1d
ezu0 <h-ezure@est.co.jp>
2022-06-13 18:56:43 +0900

    model:Producerモデルの追加

---
+++ Cinema0613/Models/Producer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cinema0613.Models
+{
+    public class Producer
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string Fullname { get; set; }
+        public string Bio { get; set; }
+    }
+}
